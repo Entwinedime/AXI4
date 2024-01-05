c4a88e835b12e2a34084031c3486264b295237b1
Entwinedime <2389117308@qq.com>
2024-01-06 00:03:29 +0800

    c file modified

--- AXI4_CROSSBAR/sim/csrc/include/axi4_transaction.h
+++ AXI4_CROSSBAR/sim/csrc/include/axi4_transaction.h
@@ -36,10 +36,10 @@
         std::vector<uint64_t>           read_buffer;
     } axi4_read_transaction;
 
-    typedef std::vector<axi4_write_transaction> read_transaction_list;
-    typedef std::vector<axi4_read_transaction> write_transaction_list;
+    typedef std::vector<axi4_read_transaction> read_transaction_list;
+    typedef std::vector<axi4_write_transaction> write_transaction_list;
 
-    int list_search_with_id(const read_transaction_list& list, uint8_t id, axi4_write_transaction_state state);
-    int list_search_with_id(const write_transaction_list& list, uint8_t id, axi4_read_transaction_state state);
+    int list_search_with_id(const read_transaction_list& list, uint8_t id, axi4_read_transaction_state state);
+    int list_search_with_id(const write_transaction_list& list, uint8_t id, axi4_write_transaction_state state);
 
 #endif
